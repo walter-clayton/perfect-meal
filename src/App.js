9be0f17a8fff961ed403e628dd31ef51edac1e25
yassinAssecoum <yassin.assecoum@hotmail.com>
2019-12-22 19:15:38 +0100

    Create App

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Home from './Components/Home/Home'
+function App() {
+  return (
+    <div className="App">
+     <Home />
+    </div>
+  );
+}
+
+export default App;
