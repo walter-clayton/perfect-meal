ed74e5d7af1fccf999b1a20294d82592990ec6e8
yassinAssecoum <yassin.assecoum@hotmail.com>
2019-12-30 16:00:25 +0100

    Home Finished with selections

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -4,6 +4,7 @@ import Button from '@material-ui/core/Button';
 import './Home.css'
 import Item from '../Item/Item'
 import logo from  './dish.svg'
+import Autocomplete from '@material-ui/lab/Autocomplete';
 
 export class Home extends Component {
     constructor(){
@@ -18,7 +19,7 @@ export class Home extends Component {
         }
     }
      search(){
-        fetch('https://api.nutritionix.com/v1_1/search/'+this.state.input+'?brand_id=513fcc648110a4cafb90ca5e&results=0%3A20&cal_min=0&cal_max=50000&appId=bfa58bb1&appKey=32f09ffb63689c776bfa016ffc977997')
+        fetch('https://api.nutritionix.com/v1_1/search/'+document.getElementById('free').value+'?brand_id=513fcc648110a4cafb90ca5e&results=0%3A20&cal_min=0&cal_max=50000&appId=bfa58bb1&appKey=32f09ffb63689c776bfa016ffc977997')
         .then((res)=>{
             if(res.status ===200){
                 return res.json()
@@ -40,8 +41,6 @@ export class Home extends Component {
 
             //Setting the id for sending to Item Component
             console.log(data.hits[0])
-            let a = 5;
-            console.log(++a)
          this.setState({
                 id:data.hits[0].fields.item_id,
                 name:data.hits[0].fields.item_name
@@ -49,6 +48,7 @@ export class Home extends Component {
             this.find();
         })
     }
+
     find(){
             fetch('https://api.nutritionix.com/v1_1/item?id='+this.state.id+'&brand_name=USDA&appId=bfa58bb1&appKey=fc5f0b60ea3bd985a02603ac239c2c10')
                 .then((res)=>{
@@ -58,18 +58,47 @@ export class Home extends Component {
                         return false;
                     }
                 })
-                .then((data)=>{
+                .then((data,err)=>{
                     console.log(data)
-                    this.setState({
+                   try{this.setState({
                         item:{
-                            cal:data.usda_fields.ENERC_KCAL.value,
+                            cal:data.nf_calories,
                             prot:data.usda_fields.PROCNT.value,
                             fat:data.usda_fields.FAT.value,
                             glu:data.usda_fields.CHOCDF.value,
                             tot:data.usda_fields.PROCNT.value+data.usda_fields.FAT.value+data.usda_fields.CHOCDF.value    
                         }
-                    })
+                    })}
+                    catch(err){
+                        console.log(err);
+                    } 
                 })
+    }
+    getFive(value){
+        
+        this.setState({
+            five:[]
+        })
+        console.log(value)
+        fetch('https://api.nutritionix.com/v1_1/search/'+value+'?brand_id=513fcc648110a4cafb90ca5e&results=0%3A20&cal_min=0&cal_max=5000&appId=bfa58bb1&appKey=32f09ffb63689c776bfa016ffc977997')
+        .then((res)=>{
+            if(res.status ===200){
+                return res.json()
+            }else{
+                return false;
+            }
+        })
+        .then((data)=>{
+            let five=[]
+            for(var i=0;i<=5;i++){
+                five.push(data.hits[i].fields.item_name)
+            }
+            this.setState({
+                five:five,
+                input:value
+            })
+
+        })
     }
     render() {
         console.log(this.state)
@@ -77,11 +106,22 @@ export class Home extends Component {
             <div id="home">
                 <h1 className="title" > The Perfect Meal</h1>
                 <img id="logo" src={logo}></img>
-                <TextField value={this.state.input} onChange={e => this.setState({input:e.target.value})} id="outlined-basic" label="examples:'egg','bacon',..." variant="outlined" color="secondary" />
+                <div id="form">
+                <Autocomplete
+                    id="free"
+                    freeSolo
+                    options={this.state.five.map(item => item)}
+                    renderInput={params => (
+                        <TextField id="input"{...params} label="examples:'egg','bacon...'" onChange={(e)=> this.getFive(e.target.value)} margin="normal" color="secondary"  variant="outlined" />
+                    )}
+                />
+
                 <Button variant="outlined" color="secondary" onClick={()=> this.search()}>
                     Search
                 </Button>
+                </div>
                 <Item name={this.state.name} item={this.state.item} />
+                
             </div>
         )
     }
