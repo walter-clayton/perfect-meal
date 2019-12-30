391f34eda23490a50bc5e1445358db59dd2aa3c1
yassinAssecoum <yassin.assecoum@hotmail.com>
2019-12-30 10:29:26 +0100

    Item Comp Finished

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -18,7 +18,7 @@ export class Home extends Component {
         }
     }
      search(){
-        fetch('https://api.nutritionix.com/v1_1/search/'+this.state.input+'?results=0%3A20&cal_min=0&cal_max=50000&fields=item_name%2Cbrand_name=Unknown%2Citem_id%2Cbrand_id=526831133203b9c3390001c8&appId=bfa58bb1&appKey=32f09ffb63689c776bfa016ffc977997')
+        fetch('https://api.nutritionix.com/v1_1/search/'+this.state.input+'?brand_id=513fcc648110a4cafb90ca5e&results=0%3A20&cal_min=0&cal_max=50000&appId=bfa58bb1&appKey=32f09ffb63689c776bfa016ffc977997')
         .then((res)=>{
             if(res.status ===200){
                 return res.json()
@@ -39,13 +39,40 @@ export class Home extends Component {
             // five=[]
 
             //Setting the id for sending to Item Component
+            console.log(data.hits[0])
+            let a = 5;
+            console.log(++a)
          this.setState({
                 id:data.hits[0].fields.item_id,
                 name:data.hits[0].fields.item_name
             })
+            this.find();
         })
     }
+    find(){
+            fetch('https://api.nutritionix.com/v1_1/item?id='+this.state.id+'&brand_name=USDA&appId=bfa58bb1&appKey=fc5f0b60ea3bd985a02603ac239c2c10')
+                .then((res)=>{
+                    if(res.status ===200){
+                        return res.json()
+                    }else{
+                        return false;
+                    }
+                })
+                .then((data)=>{
+                    console.log(data)
+                    this.setState({
+                        item:{
+                            cal:data.usda_fields.ENERC_KCAL.value,
+                            prot:data.usda_fields.PROCNT.value,
+                            fat:data.usda_fields.FAT.value,
+                            glu:data.usda_fields.CHOCDF.value,
+                            tot:data.usda_fields.PROCNT.value+data.usda_fields.FAT.value+data.usda_fields.CHOCDF.value    
+                        }
+                    })
+                })
+    }
     render() {
+        console.log(this.state)
         return (
             <div id="home">
                 <h1 className="title" > The Perfect Meal</h1>
@@ -54,8 +81,7 @@ export class Home extends Component {
                 <Button variant="outlined" color="secondary" onClick={()=> this.search()}>
                     Search
                 </Button>
-                <Item id={this.state.id} />
-                {this.state.five ? this.state.five :"Rien"}
+                <Item name={this.state.name} item={this.state.item} />
             </div>
         )
     }
--- src/Components/Item/Item.js
+++ src/Components/Item/Item.js
@@ -12,49 +12,34 @@ constructor(){
     }
 }
 
-find(){
-    if(this.props.id==""){
-        console.log("pas d'id")
-    }else{
-        console.log("id")
-        fetch('https://api.nutritionix.com/v1_1/item?id='+this.props.id+'&appId=bfa58bb1&appKey=32f09ffb63689c776bfa016ffc977997')
-            .then((res)=>{
-                if(res.status ===200){
-                    return res.json()
-                }else{
-                    return false;
-                }
-            })
-            .then((data)=>{
-                console.log(data)
-            })
-    }
-}
     render() {
-        this.find()
+        console.log("props item"+this.props.item)
         return (
             <div>
                 <div id="header">
-                    <h1>Enter an Item</h1>
-                    33%
+        {this.props.name !="" ? <h1>{ this.props.name}</h1> : <h1>Enter an Item</h1>}
+        {this.props.item ? <p id="cal">{this.props.item.cal} kcal</p> : ""}  
                 </div>
                 <div id="nutriments">
                     <div>
                     <img src={carbs}></img>
                     <p>1. Carbs</p>
+                    {this.props.item ? <p>{Math.floor((this.props.item.glu/this.props.item.tot)*100)}%</p> : ""}
                     </div>
                     <div>
                     <img src={carbs}></img>
                     <p>2. Proteins</p>
+                    {this.props.item ? <p>{Math.floor((this.props.item.prot/this.props.item.tot)*100)}%</p> : ""}
                     </div>
                     <div>
                     <img src={carbs}></img>
                     <p>3. Fat</p>
+                    {this.props.item ? <p>{Math.floor((this.props.item.fat/this.props.item.tot)*100)}%</p> : ""}
                     </div>
                 </div>
             </div>
         )
-    }
+    }   
 }
 
 export default Item
